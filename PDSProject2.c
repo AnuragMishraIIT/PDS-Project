c8a3afa0b4197dba8ac3db021ff5767119bd7935
AnuragMishraIIT <157116741+AnuragMishraIIT@users.noreply.github.com>
2024-03-22 11:00:52 +0530

    Update(Friday 11am) PDSProject2.c

--- PDSProject2.c
+++ PDSProject2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
 const unsigned char sbox[16][16] = // Tushar Jawane
     //     0     1     2    3      4     5     6     7     8     9     a     b     c    d      e     f
     {
@@ -22,9 +23,30 @@ const unsigned char sbox[16][16] = // Tushar Jawane
         {0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16}  // f
 };
 
+const unsigned char inSbox[16][16] =
+    //     0     1     2    3      4     5     6     7     8     9     a     b     c    d      e     f
+    {
+        {0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb}, // 0
+        {0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb}, // 1
+        {0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d, 0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e}, // 2
+        {0x08, 0x2e, 0xa1, 0x66, 0x28, 0xd9, 0x24, 0xb2, 0x76, 0x5b, 0xa2, 0x49, 0x6d, 0x8b, 0xd1, 0x25}, // 3
+        {0x72, 0xf8, 0xf6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xd4, 0xa4, 0x5c, 0xcc, 0x5d, 0x65, 0xb6, 0x92}, // 4
+        {0x6c, 0x70, 0x48, 0x50, 0xfd, 0xed, 0xb9, 0xda, 0x5e, 0x15, 0x46, 0x57, 0xa7, 0x8d, 0x9d, 0x84}, // 5
+        {0x90, 0xd8, 0xab, 0x00, 0x8c, 0xbc, 0xd3, 0x0a, 0xf7, 0xe4, 0x58, 0x05, 0xb8, 0xb3, 0x45, 0x06}, // 6
+        {0xd0, 0x2c, 0x1e, 0x8f, 0xca, 0x3f, 0x0f, 0x02, 0xc1, 0xaf, 0xbd, 0x03, 0x01, 0x13, 0x8a, 0x6b}, // 7
+        {0x3a, 0x91, 0x11, 0x41, 0x4f, 0x67, 0xdc, 0xea, 0x97, 0xf2, 0xcf, 0xce, 0xf0, 0xb4, 0xe6, 0x73}, // 8
+        {0x96, 0xac, 0x74, 0x22, 0xe7, 0xad, 0x35, 0x85, 0xe2, 0xf9, 0x37, 0xe8, 0x1c, 0x75, 0xdf, 0x6e}, // 9
+        {0x47, 0xf1, 0x1a, 0x71, 0x1d, 0x29, 0xc5, 0x89, 0x6f, 0xb7, 0x62, 0x0e, 0xaa, 0x18, 0xbe, 0x1b}, // a
+        {0xfc, 0x56, 0x3e, 0x4b, 0xc6, 0xd2, 0x79, 0x20, 0x9a, 0xdb, 0xc0, 0xfe, 0x78, 0xcd, 0x5a, 0xf4}, // b
+        {0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31, 0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f}, // c
+        {0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef}, // d
+        {0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61}, // e
+        {0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d}  // f
+};
+
 const unsigned char roundconstants[11] = // Anurag Mishra
     {
-        0x00,0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80,0x1B,0x36
+        0x00, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1B, 0x36
     };
 
 // Anurag Mishra
@@ -42,7 +64,7 @@ void shiftRows(unsigned char array[][4][4], int num) // A numx4x4 array is recei
                 temp[j] = array[k][i][j];
             }
 
-            for (j = 0; j < 4 - i; j++)     
+            for (j = 0; j < 4 - i; j++)
             {
                 array[k][i][j] = array[k][i][j + i];
             }
@@ -55,30 +77,56 @@ void shiftRows(unsigned char array[][4][4], int num) // A numx4x4 array is recei
     }
 }
 
-void shiftRows2(unsigned char array[4][4]) // A numx4x4 array is received 
+void shiftRows2(unsigned char array[4][4]) // A numx4x4 array is received
 {
-	int k, i, j, index;
-	char temp[3];
+    int k, i, j, index;
+    char temp[3];
 
-	for (i = 0; i < 4; i++) // This loop takes account of the row being worked upon
-	{
-		for (j = 0; j < i; j++)
-		{
-			temp[j] = array[i][j];
-		}
+    for (i = 0; i < 4; i++) // This loop takes account of the row being worked upon
+    {
+        for (j = 0; j < i; j++)
+        {
+            temp[j] = array[i][j];
+        }
 
-		for (j = 0; j < 4-i; j++)
-		{
-			array[i][j] = array[i][j + i];
-		}
+        for (j = 0; j < 4 - i; j++)
+        {
+            array[i][j] = array[i][j + i];
+        }
 
-		
-		for ( index = 0,j = 4 - i; j < 4; j++, index++)
-		{
-           
-			array[i][j] = temp[index];
-		}
-	}
+        for (index = 0, j = 4 - i; j < 4; j++, index++)
+        {
+
+            array[i][j] = temp[index];
+        }
+    }
+}
+
+void inShiftRows(unsigned char array[][4][4], int num) // A numx4x4 array is received
+{
+    int k, i, j, index;
+    char temp[3];
+
+    for (k = 0; k < num; k++) // This loop runs for each 4x4 block.
+    {
+        for (i = 0; i < 4; i++) // This loop takes account of the row being worked upon
+        {
+            for (index = 0, j = 4 - i; j < 4; index++, j++)
+            {
+                temp[index] = array[k][i][j];
+            }
+
+            for (j = 3 - i; j >=0; j--)
+            {
+                array[k][i][j + i] = array[k][i][j];
+            }
+
+            for (j =0; j < i; j++)
+            {
+                array[k][i][j] = temp[j];
+            }
+        }
+    }
 }
 
 void rotword(unsigned char temp_vector[]) // Anurag Mishra
@@ -95,24 +143,37 @@ void subword(unsigned char temp_vector[]) // Anurag Mishra
 {
     for (int i = 0; i < 4; i++) // Hexadecimal digit of the form 0x(XY)
     {
-        int Y = temp_vector[i] % 16;
-        int X = temp_vector[i] / 16;
+        int Y = temp_vector[i] & ((1 << 4) - 1);
+        int X = (temp_vector[i]) >> 4;
 
         temp_vector[i] = sbox[X][Y];
     }
 }
 
-void subBytes(unsigned char temp_vector[4][4]) //Anurag Mishra
+void subBytes(unsigned char temp_vector[4][4]) // Anurag Mishra
 {
-	for(int row=0;row<4;row++)
-	{
-		for(int col=0;col<4;col++) //Hexadecimal digit of the form 0x(XY)
-		{
-		int Y=temp_vector[row][col]%16; 
-		int X=temp_vector[row][col]/16;
-		temp_vector[row][col]=sbox[X][Y];
-		}
-	}
+    for (int row = 0; row < 4; row++)
+    {
+        for (int col = 0; col < 4; col++) // Hexadecimal digit of the form 0x(XY)
+        {
+            int Y = temp_vector[row][col] & ((1 << 4) - 1);
+            int X = temp_vector[row][col] >> 4;
+            temp_vector[row][col] = sbox[X][Y];
+        }
+    }
+}
+
+void insubBytes(unsigned char temp_vector[4][4]) // Anurag Mishra
+{
+    for (int row = 0; row < 4; row++)
+    {
+        for (int col = 0; col < 4; col++) // Hexadecimal digit of the form 0x(XY)
+        {
+            int Y = temp_vector[row][col] & ((1 << 4) - 1);
+            int X = temp_vector[row][col] >> 4;
+            temp_vector[row][col] = inSbox[X][Y];
+        }
+    }
 }
 
 void roundConst(unsigned char temp_vector[], int keynum) // Anurag Mishra
@@ -161,36 +222,46 @@ void keySchedule(unsigned char r_keys[][4][4], unsigned char ciph_key[4][4]) // 
     }
 }
 
-unsigned char multiply(unsigned char a, unsigned char b) {
+unsigned char multiply(unsigned char a, unsigned char b)
+{ // Note: Bits numbering starts from the right to the left from 0 to 7 (since there are only 8 bits in char)
     unsigned char p = 0;
     unsigned char high_bit_set;
-    for (int i = 0; i < 8; ++i) {
-        if (b & 1) {
+    for (int i = 0; i < 8; i++)
+    {
+        if ((b & 1) != 0)
+        {
             p ^= a;
         }
-        high_bit_set = a & 0x80;
-        a <<= 1;
-        if (high_bit_set) {
+        high_bit_set = a & 0x80; // Checks if the 7th bit is on or off for 'a'
+
+        a <<= 1; // Soon 'b' will be right shifted to cancel effects in polynomial multiplication. Old 7th bit is removed.
+
+        if (high_bit_set != 0) // If 7th bit of old 'a' (Or the 8th bit of new 'a') was (is) on.
+        {
             a ^= 0x1B; // 0x1B is the irreducible polynomial in GF(2^8)
+            // 0x1B is equivalent to x^8 + x^4 + x^3 + x + 1
+            // Toggles the values corresponding to the 4th,3rd,1st and 0th bits (Which is irreducible polynomial theorem itself)
         }
-        b >>= 1;
+        b >>= 1; // Cancels effect of shifting of 'a' in polynomial multiplication
     }
     return p;
 }
 
-void mixcolumns(unsigned char state[4][4]) {
-    unsigned char matrix[4][4] = 
-    {
-        {0x02, 0x03, 0x01, 0x01},
-        {0x01, 0x02, 0x03, 0x01},
-        {0x01, 0x01, 0x02, 0x03},
-        {0x03, 0x01, 0x01, 0x02}
-    };
+void mixcolumns(unsigned char state[4][4])
+{
+    unsigned char matrix[4][4] =
+        {
+            {0x02, 0x03, 0x01, 0x01},
+            {0x01, 0x02, 0x03, 0x01},
+            {0x01, 0x01, 0x02, 0x03},
+            {0x03, 0x01, 0x01, 0x02}};
 
     unsigned char result[4][4];
 
-    for (int i = 0; i < 4; ++i) {
-        for (int j = 0; j < 4; ++j) {
+    for (int i = 0; i < 4; ++i)
+    {
+        for (int j = 0; j < 4; ++j)
+        {
             result[i][j] = multiply(matrix[i][0], state[0][j]) ^
                            multiply(matrix[i][1], state[1][j]) ^
                            multiply(matrix[i][2], state[2][j]) ^
@@ -198,35 +269,65 @@ void mixcolumns(unsigned char state[4][4]) {
         }
     }
     // Copy result back to state matrix
-    for (int i = 0; i < 4; ++i) {
-        for (int j = 0; j < 4; ++j) {
+    for (int i = 0; i < 4; ++i)
+    {
+        for (int j = 0; j < 4; ++j)
+        {
             state[i][j] = result[i][j];
         }
     }
 }
 
-void addRoundKey(unsigned char ptext[4][4], unsigned char roundKey[4][4]) 
+void addRoundKey(unsigned char ptext[4][4], unsigned char roundKey[4][4])
 {
-	for (int col = 0; col < 4; col++) {
-		for (int row = 0; row < 4; row++)
-		{
-			ptext[row][col] ^= roundKey[row][col];
-		}
-	}
+    for (int col = 0; col < 4; col++)
+    {
+        for (int row = 0; row < 4; row++)
+        {
+            ptext[row][col] ^= roundKey[row][col];
+        }
+    }
 }
 
-void plain_text_block_generate(char* txt,unsigned char ptext[][4][4],int blocks)
+void plain_text_block_generate(char *txt, unsigned char ptext[][4][4], int blocks)
 {
-    for(int i=0,c=0;i<blocks;i++)
-	{
-		for(int row=0;row<4;row++)
-		{
-			for(int col=0;col<4;col++)
-			{
-				ptext[i][row][col] = txt[c++];
-			}
-		}
-	}
+    for (int i = 0, c = 0; i < blocks; i++)
+    {
+        for (int row = 0; row < 4; row++)
+        {
+            for (int col = 0; col < 4; col++)
+            {
+                ptext[i][row][col] = txt[c++];
+            }
+        }
+    }
+}
+// Work n progress
+void inverseShiftRows(unsigned char array[][4][4], int num) // A numx4x4 array is received
+{
+    int k, i, j, index;
+    char temp[3];
+
+    for (k = 0; k < num; k++) // This loop runs for each 4x4 block.
+    {
+        for (i = 0; i < 4; i++) // This loop takes account of the row being worked upon
+        {
+            for (index = 0, j = 4 - i; j < 4; j++, index++)
+            {
+                temp[index] = array[k][i][j];
+            }
+
+            for (j = 3; j > index; j--)
+            {
+                array[k][i][j] = array[k][i][j - i];
+            }
+
+            for (index = 0, j = 4 - i; j < 4; j++, index++)
+            {
+                array[k][i][j] = temp[index];
+            }
+        }
+    }
 }
 int main()
 {
@@ -237,70 +338,68 @@ int main()
             {0xAB, 0x5F, 0x72, 0xE3},
             {0x94, 0x2D, 0x8A, 0x6C},
             {0x3E, 0x11, 0x9B, 0xD8},
-            {0x7A, 0x45, 0xF2, 0x81}
-        };
-    char* cipher_key = "abcdefghijklmnop";
+            {0x7A, 0x45, 0xF2, 0x81}};
+    char *cipher_key = "abcdefghijklmnop";
     unsigned char cipher_key_final[4][4];
-    int c=0;
-    for(int row=0;row<4;row++)
-	{
-		for(int col=0;col<4;col++)
-		{
-			cipher_key_final[row][col] = cipher_key[c++];
-		}
-	}
+    int c = 0;
+    for (int row = 0; row < 4; row++)
+    {
+        for (int col = 0; col < 4; col++)
+        {
+            cipher_key_final[row][col] = cipher_key[c++];
+        }
+    }
     keySchedule(roundkeys, cipher_key_final);
 
-    char* str=(char*)malloc(4096*sizeof(char));
-    scanf("%[^\n]s",str);
-    str=realloc(str,(strlen(str)+1)*sizeof(char));
+    char *str = (char *)malloc(4096 * sizeof(char));
+    scanf("%[^\n]s", str);
+    str = realloc(str, (strlen(str) + 1) * sizeof(char));
 
-	int blocks=strlen(str)/16 ;
-	unsigned char ptext[blocks][4][4];
-	plain_text_block_generate(str,ptext,blocks);
-	
+    int blocks = strlen(str) / 16;
+    unsigned char ptext[blocks][4][4];
+    plain_text_block_generate(str, ptext, blocks);
 
-	for (int k = 0; k < blocks; k++)
-	{
-		printf("\nArray %d:\n\n", k + 1);
-		for (int i = 0; i < 4; i++)
-		{
-			for (int j = 0; j < 4; j++)
-				printf("%c ", ptext[k][i][j]);
-			printf("\n");
-		}
-		printf("\n");
-	}
-	
-	for(int current_block=0;current_block<blocks;current_block++)
-	{
-		
-		addRoundKey(ptext[current_block], roundkeys[0]);
-		// Main rounds
-		for (int round = 1; round < 10; round++)
-		{
-			subBytes(ptext[current_block]);
-			shiftRows2(ptext[current_block]);
-			mixcolumns(ptext[current_block]);
-			addRoundKey(ptext[current_block], roundkeys[round]);
-		}
+    for (int k = 0; k < blocks; k++)
+    {
+        printf("\nArray %d:\n\n", k + 1);
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+                printf("%c ", ptext[k][i][j]);
+            printf("\n");
+        }
+        printf("\n");
+    }
 
-		// Final round
-		subBytes(ptext[current_block]);
-		shiftRows2(ptext[current_block]);
-		addRoundKey(ptext[current_block], roundkeys[10]);
-	}
-	for (int k = 0; k < blocks; k++)
-	{
-		printf("\nArray %d:\n\n", k + 1);
-		for (int i = 0; i < 4; i++)
-		{
-			for (int j = 0; j < 4; j++)
-				printf("%x ", ptext[k][i][j]);
-			printf("\n");
-		}
-		printf("\n");
-	}
+    for (int current_block = 0; current_block < blocks; current_block++)
+    {
+
+        addRoundKey(ptext[current_block], roundkeys[0]);
+        // Main rounds
+        for (int round = 1; round < 10; round++)
+        {
+            subBytes(ptext[current_block]);
+            shiftRows2(ptext[current_block]);
+            mixcolumns(ptext[current_block]);
+            addRoundKey(ptext[current_block], roundkeys[round]);
+        }
+
+        // Final round
+        subBytes(ptext[current_block]);
+        shiftRows2(ptext[current_block]);
+        addRoundKey(ptext[current_block], roundkeys[10]);
+    }
+    for (int k = 0; k < blocks; k++)
+    {
+        printf("\nArray %d:\n\n", k + 1);
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+                printf("%x ", ptext[k][i][j]);
+            printf("\n");
+        }
+        printf("\n");
+    }
     free(str);
     return 0;
 }
